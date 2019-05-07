24521dcb5f0dd7c9aca37c7045b55648a5cb28b7
nydiegoa <49658730+nydiegoa@users.noreply.github.com>
2019-05-06 20:20:31 -0400

    video

---
+++ adobe/index.js
@@ -0,0 +1,22 @@
+//<a href='' class='link'>
+
+//const button3 = document.querySelector('.button3');
+//const content3 = "<p>Thanks Pam</p>";
+//button3.addEventListener("click", e=> {
+//  placeholder.innerHTML = content3;
+//})
+
+const i1 = document.querySelector('.item1');
+const i2 = document.querySelector('.item2');
+const i3 = document.querySelector('.item3');
+const i4 = document.querySelector('.item4');
+const i5 = document.querySelector('.item5');
+
+const bo1 = document.querySelector('.infoHead');
+const bo2 = document.querySelector('.video1');
+const bo3 = document.querySelector('.info');
+
+const c1 = "<h1> intro2 </h1>"
+
+i2.addEventListener("click", e=> {bo1.innerHTML = c1;})
+
