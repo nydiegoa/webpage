8707b1782afac747dddf75daf6592f1c2303138e
nydiegoa <49658730+nydiegoa@users.noreply.github.com>
2019-05-12 19:17:41 -0400

    Page Finish

--- adobe/index.js
+++ adobe/index.js
@@ -28,19 +28,19 @@ const b5 = '<h1>Putting it Together</h1>';
 
 // Video
 
-const c1 = '<iframe width="710" height="390" src="https://www.youtube.com/embed/bDYWUGCgKCI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-const c2 = '<iframe width="710" height="390" src="https://www.youtube.com/embed/s7JwxPnYoOw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-const c3 = '';
-const c4 = '';
-const c5 = '';
+const c1 = '<div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe src="https://www.loom.com/embed/f31d5b8946ef4c5980ea43ed3150e9c8" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>';
+const c2 = '<div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe src="https://www.loom.com/embed/fd238097570241bfa3e2a6711e34635e" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>';
+const c3 = '<div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe src="https://www.loom.com/embed/e3cfaf4105ca4307912819c6250bf106" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>';
+const c4 = '<div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe src="https://www.loom.com/embed/19952dcaee9b423c9393efb7697c2157" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>';
+const c5 = '<div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe src="https://www.loom.com/embed/fd4d309ae0df4e09875f74958f166844" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>';
 
 // Info
 
-const d1 = '<h1>What to remember</h1>';
-const d2 = '<h1>Here are some basic tools to remember</h1>';
-const d3 = '<h1>How to use the tools with your work</h1>';
-const d4 = '<h1>What are vectors and how they change your work</h1>';
-const d5 = '<h1>Now lets put everything we learned together</h1>';
+const d1 = '<h1> Introduction <br> <br> In our Introduction video we go over the very basics of adobe Illustrator.</h1>';
+const d2 = '<h1> The Adobe Illustrator interface <br><br> We go over how to open windows, how to make use of our file creation and exporting, and how to edit objects. </h1>';
+const d3 = '<h1>Here are some basic tools to remember <br><br> The direct selection tool which allows you to edit anchor points, and the pen tool to create vectors.</h1>';
+const d4 = '<h1>How to use your tools <br><br> The pen tool is the most commonly used tool to create shapes also the direct selection tool in combination.</h1>';
+const d5 = '<h1>Now lets put everything we learned together <br><br> Here we create a basic butterfly in the sky with a nice background.</h1>';
 
 // Infohead box placement
 
